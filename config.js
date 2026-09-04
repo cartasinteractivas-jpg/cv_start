7e74bcc2191f57c9a9ee93f1ec6dc04ec86c4752
cartasinteractivas-jpg <cartasinteractivas@gmail.com>
2026-09-03 22:39:34 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/* Cambia estas direcciones antes de publicar cv_start. No coloques claves ni tokens en este archivo. */
+window.WEBPOCKET_CONFIG = {
+  cvAdminUrl: "../cv_admin/?registro=1",
+  apiBaseUrl: "https://TU-DOMINIO-CV-ADMIN.example"
+};
